6dcb9ca03b6f07013c1496d969785e52e28e4ef9
Stacha Clemens <stacha@MacBook-Pro-2.local>
2022-08-26 11:52:11 +1000

    Updates

---
+++ precache-manifest.70ccf7cc32fecb18d7c9260f2d433ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84d66785538d470a56993268813fb4b",
+    "url": "/customer-reviews/index.html"
+  },
+  {
+    "revision": "4fd448f4ccdb7b9b7cdc",
+    "url": "/customer-reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "9cf77318e524bbf5d860",
+    "url": "/customer-reviews/static/js/2.aab8ba5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/customer-reviews/static/js/2.aab8ba5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd448f4ccdb7b9b7cdc",
+    "url": "/customer-reviews/static/js/main.26188605.chunk.js"
+  },
+  {
+    "revision": "93c6300a6af1db1b6e9b",
+    "url": "/customer-reviews/static/js/runtime-main.3e1931f2.js"
+  }
+]);
